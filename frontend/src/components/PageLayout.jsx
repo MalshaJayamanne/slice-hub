513c4ac543ad840d9a6f1088bf26a5c41ee865a8
kavindup20010305 <kavindup52@gmail.com>
2026-04-23 14:38:30 +0530

    Add cart context and wire food screens

--- frontend/src/components/PageLayout.jsx
+++ frontend/src/components/PageLayout.jsx
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import { useCart } from "../context/CartContext";
 
 function PageLayout() {
+  const { cartCount } = useCart();
+
   return (
     <div>
-      <Navbar />
+      <Navbar cartCount={cartCount} />
 
       <main className="min-h-screen p-4">
         <Outlet />
